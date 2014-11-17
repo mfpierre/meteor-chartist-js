ad979f22d3d540d6e256825dc7ddc4fb62909121
Pierre <mfpierre@gmail.com>
2014-11-17 01:55:05 +0100

    Bump version to reflect chartist update

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'mfpierre:chartist-js',
   summary: 'Simple responsive charts',
-  version: '1.0.0',
+  version: '1.1.0',
   git: 'https://github.com/mfpierre/meteor-chartist-js'
 });
 
