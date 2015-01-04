5237d502f3935fc973ebcb9a4967fb46710ce031
Chris Slack <slackcc@gmail.com>
2015-01-04 17:09:18 -0500

    Updated chartist-js lib to 0.5 version and package.js to reflect new file structure

--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 Package.describe({
-  name: 'mfpierre:chartist-js',
+  name: 'slackcc:chartist-js',
   summary: 'Simple responsive charts',
   version: '1.1.0',
-  git: 'https://github.com/mfpierre/meteor-chartist-js'
+  git: 'https://github.com/slackcc/meteor-chartist-js'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
   api.addFiles([
-    'chartist-js/libdist/chartist.min.js',
-    'chartist-js/libdist/chartist.min.css'
+    'chartist-js/dist/chartist.min.js',
+    'chartist-js/dist/chartist.min.css'
   ], 'client');
 });
