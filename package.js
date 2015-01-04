b3aba71aed9706ff2077af3de730e67405ad2162
Chris Slack <slackcc@gmail.com>
2015-01-04 17:32:44 -0500

    Update chartist-js to v0.5.0

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'mfpierre:chartist-js',
   summary: 'Simple responsive charts',
   version: '1.2.0',
-  git: 'https://github.com/slackcc/meteor-chartist-js'
+  git: 'https://github.com/mfpierre/meteor-chartist-js'
 });
 
 Package.onUse(function(api) {
