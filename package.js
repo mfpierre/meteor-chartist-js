db8480cf6b29447068485405fd782d1de11e1082
Pierre <mfpierre@gmail.com>
2015-01-29 18:10:38 +0100

    Update Chartist to v0.6.1

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'mfpierre:chartist-js',
   summary: 'Simple responsive charts',
-  version: '1.3.0',
+  version: '1.3.1',
   git: 'https://github.com/mfpierre/meteor-chartist-js'
 });
 
